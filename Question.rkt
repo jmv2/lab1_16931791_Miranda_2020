78e874c743479168d1463afecd065cf80e13ad7d
Juan Miranda Cárdenas <jmirandav2@gmail.com>
2020-10-25 23:08:17 -0300

    Nuevos archivos de TDAs: Answer & Question

---
+++ Question.rkt
@@ -0,0 +1,3 @@
+#lang racket
+
+; TDA Question 
--- Stack.rkt
+++ Stack.rkt
@@ -3,13 +3,3 @@
 (require "Usuario.rkt" "main.rkt")
 
 ; TDA Stack
-
-(define (Stack stack)
-  (lambda (user)
-    (list stack user)))
-
-
-
-
-
-(define testStack (Stack null))
